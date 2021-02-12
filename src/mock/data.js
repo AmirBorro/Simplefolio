72cbb3f0691e00eaca4862763c0f0ad012938090
Amir <amirborro@gmail.com>
2021-02-12 14:41:05 +0100

    Version 1.0

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,39 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Amir Borro | Front-end web developer', // e.g: 'Name | Developer'
+  lang: 'en@', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Amir Borro',
+  subtitle: "I'm a React developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile1.jpg',
+  paragraphOne:
+    'After an extensive career in events management and production, I decided to shift to what really passionate me: programming!',
+  paragraphTwo: 'I started by learning WordPress and quickly dived deeper into React.js',
+  paragraphThree: "If you'd like to work with me, have a look at my resume and let's get in touch",
+  resume: 'https://docdro.id/QdwXKJT', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'BoxingFactoryProject.jpeg',
+    title: 'Boxing Factory',
+    info: 'Website built using WordPress.',
+    info2: 'E-commerce, multi-lingual, integration of a third-party booking software.',
+    url: 'wwww.boxingfactory.be',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +42,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'hello@amirborro.com',
 };
 
 // FOOTER DATA
@@ -67,28 +50,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'www.linkedin.com/in/amirborro',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/AmirBorro',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
